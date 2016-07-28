d80835174eb576d8afc7d0b20a4b3fec1839f049
AleksandrMishchaniuk <map.m@ua.fm>
2016-07-28 22:41:47 +0300

    added user settings

--- app/controllers/user/settings_controller.rb
+++ app/controllers/user/settings_controller.rb
@@ -1,2 +1,77 @@
 class User::SettingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    define_variables
+  end
+
+  def billing_address
+    (current_user.billing_address)? current_user.billing_address.update(address_params :billing):
+                                    current_user.billing_address = Address.create(address_params :billing)
+    if current_user.billing_address.errors.empty?
+      redirect_to edit_user_settings_path
+    else
+      define_variables
+      render :edit
+    end
+  end
+
+  def shipping_address
+    (current_user.shipping_address)? current_user.shipping_address.update(address_params :shipping):
+                                     current_user.shipping_address = Address.create(address_params :shipping)
+    if current_user.shipping_address.errors.empty?
+      redirect_to edit_user_settings_path
+    else
+      define_variables
+      render :edit
+    end
+  end
+
+  def email
+    if current_user.update(user_params)
+      redirect_to edit_user_settings_path
+    else
+      current_user.reload
+      define_variables
+      render :edit
+    end
+  end
+
+  def password
+    if current_user.update_with_password(user_params)
+      sign_in current_user, bypass: true
+      redirect_to edit_user_settings_path, notice: 'password was changed'
+    else
+      current_user.reload
+      define_variables
+      render :edit
+    end
+  end
+
+  def remove_user
+    if params[:confirm] && current_user.destroy
+      redirect_to new_user_session_path, notice: 'account was deleted'
+    else
+      redirect_to edit_user_settings_path, alert: "You should check \"I understand that all data will be lost\""  
+    end
+  end
+
+  protected
+
+  def address_params(type)
+    params.require(:user).require(type).require(:address).permit(:first_name, :last_name,
+          :address_line, :city, :country, :zip, :phone)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :current_password, :password, :password_confirmation)
+  end
+
+  def define_variables
+    @user ||= current_user
+    @billing_address ||= current_user.billing_address || Address.new
+    @shipping_address ||= current_user.shipping_address || Address.new
+    @email ||= current_user.email
+  end
+
 end
