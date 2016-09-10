72d1a3ab1e639b54a56fc433b7cbb45ed2551e60
AleksandrMishchaniuk <map.m@ua.fm>
2016-09-10 18:49:09 +0300

    added tests for model Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.items_for_navigation' do
+    let(:names) { [ 'category 1', 'category 2', 'category 3' ] }
+    let(:categories) { names.map { |name| create(:category, name: name) } }
+    let(:expectation) do
+      <<-RES
+category.item "category 1", "category 1", '/'+I18n.locale.to_s+'/shop/categories/#{categories[0].id}', html: {class: 'some_class'}
+ category.item "category 2", "category 2", '/'+I18n.locale.to_s+'/shop/categories/#{categories[1].id}', html: {class: 'some_class'}
+ category.item "category 3", "category 3", '/'+I18n.locale.to_s+'/shop/categories/#{categories[2].id}', html: {class: 'some_class'}
+      RES
+    end
+    before { expectation }
+    it do
+      expect(described_class.items_for_navigation('some_class')+"\n").to be_eql(expectation)
+    end
+  end
+
 end
