d0cf9e51df3a65fc096b7f6dbed266981af6819e
AleksandrMishchaniuk <map.m@ua.fm>
2016-09-22 16:51:05 +0300

    fixed mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,11 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'map-bookstore.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:                'smtp.gmail.com',
-    port:                   587,
+    port:                   465,
     domain:                 'map-bookstore.herokuapp.com',
     user_name:              ENV['SMTP_USERNAME'],
     password:               ENV['SMTP_PASSWORD'],
