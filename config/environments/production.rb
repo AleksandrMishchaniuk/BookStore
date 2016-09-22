e17bd0f9352637f824b8f73424de8230106c6be3
AleksandrMishchaniuk <map.m@ua.fm>
2016-09-22 18:25:31 +0300

    fixed mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'map-bookstore.herokuapp.com', port: '' }
+  config.action_mailer.default_url_options = { host: 'map-bookstore.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -93,3 +93,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+Rails.application.routes.default_url_options[:host] = 'map-bookstore.herokuapp.com'
