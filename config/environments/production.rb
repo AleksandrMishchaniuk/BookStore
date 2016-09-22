6d1f11bc886e364b80902a3b165ddd2b892cacad
AleksandrMishchaniuk <map.m@ua.fm>
2016-09-22 17:06:41 +0300

    fixed mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:                'smtp.gmail.com',
     port:                   465,
-    domain:                 'map-bookstore.herokuapp.com',
+    domain:                 'gmail.com',
     user_name:              ENV['SMTP_USERNAME'],
     password:               ENV['SMTP_PASSWORD'],
     authentication:         'plain',
